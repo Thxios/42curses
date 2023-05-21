6ee33e096fc3b4777bbc4ab755f30c35a08300f6
Jimyeong Lee <jimlee@c6r4s1.42seoul.kr>
2023-05-21 18:08:05 +0900

    solver header

--- push_swap/includes/solver/solver.h
+++ push_swap/includes/solver/solver.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   solver.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jimlee <jimlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jimlee <jimlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:20:11 by jimlee            #+#    #+#             */
-/*   Updated: 2023/05/10 11:20:57 by jimlee           ###   ########.fr       */
+/*   Updated: 2023/05/21 18:07:23 by jimlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SOLVER_H
-# define SOLVER
+# define SOLVER_H
 
 # include "solver/constants.h"
 # include "solver/sort.h"
