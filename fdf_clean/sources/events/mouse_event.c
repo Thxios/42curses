002a6270df46731a248c6693b038e1fc722d8c17
Jimyeong Lee <jimlee@c6r5s8.42seoul.kr>
2023-07-10 11:15:30 +0900

    fdf submit

--- fdf_clean/sources/events/mouse_event.c
+++ fdf_clean/sources/events/mouse_event.c
@@ -6,7 +6,7 @@
 /*   By: jimlee <jimlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 14:34:08 by jimlee            #+#    #+#             */
-/*   Updated: 2023/07/09 19:08:08 by jimlee           ###   ########.fr       */
+/*   Updated: 2023/07/10 10:53:10 by jimlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 #include "events/buttons.h"
 #include "camera/camera.h"
 
-void	mousedown_trigger_event(int button, int x, int y)
+void	mousedown_trigger_event(int button, int x, int y, t_upd *upd)
 {
+	(void)x;
+	(void)y;
+	(void)upd;
 	if (button == BTN_WHEELUP)
 	{
 		zoom_camera(-1);
@@ -29,5 +32,6 @@ void	mousedown_trigger_event(int button, int x, int y)
 int	mousedown_event(int button, int x, int y, t_upd *upd)
 {
 	(void)upd;
-	mousedown_trigger_event(button, x, y);
+	mousedown_trigger_event(button, x, y, upd);
+	return (0);
 }
